1d0fdaa7b10f80d301a0425262b7ec03909d3e9c
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-31 23:21:04 +0300

    sciene

---
+++ DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Controllers/ScienceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocentlikPuanHesaplama.Controllers
+{
+    public class ScienceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult pResult()
+        {
+            // yine .cshtml dosyasını render etmemizi sağlar 
+            // view resulttan temel farkı  client tabanlı yapılan 
+            //ajax isteklerinde kullanımı sağlanır bu sayede sayfayı komple değiştirmek yerine
+            // sayfanın bir kısmı değişir.
+            PartialViewResult result = PartialView();
+            return result;
+        }
+
+    }
+}
