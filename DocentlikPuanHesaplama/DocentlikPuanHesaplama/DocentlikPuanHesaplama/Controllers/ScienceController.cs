529ca95b8ae39264e5ff69a65b6fc8c5811a3ef0
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-02 04:04:54 +0300

    anser

--- DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Controllers/ScienceController.cs
+++ DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Controllers/ScienceController.cs
@@ -1,57 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocentlikPuanHesaplama.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace DocentlikPuanHesaplama.Controllers
 {
     public class ScienceController : Controller
     {
+      
+        [HttpGet]
+        public IActionResult Answer()
+        {
+            Messages m = JsonSerializer.Deserialize <Messages>(TempData["message"].ToString());
+            string[] a = new string[]{ "Hata1", "Hata2"};
+            m.ErrorMessage = a;
+            return View(m);
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Egitim()
         {
             return View();
         }
+    
+        [HttpPost]
+        public IActionResult Egitim(string[] EgitimBhatirlatici)
+        {
+            Messages m = new Messages();
+            if (EgitimBhatirlatici.Length > 1)
+            {
+                m.message = "Bilgi Var";
+            }
+            else
+            {
+                m.message = "Bilgi Yok";
+            }
+            TempData["message"] = JsonSerializer.Serialize(m);
+            return RedirectToAction("Answer");
+        }
+        [HttpGet]
         public IActionResult Fen()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Filoloji()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult GuzelSanatlar()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Hukuk()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult ilahiyat()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Mimarlik()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Muhendis()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Saglik()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult SosyalBeseri()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Ziraat()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Spor()
         {
             return View();
---
+++ DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Models/Messages.cs
@@ -0,0 +1,26 @@
+﻿namespace DocentlikPuanHesaplama.Models
+{
+    public class Messages
+    {
+        public string message { get; set; }
+        public List<ListMadde> maddeler { get; set; }
+
+
+        public string ToplamDoktoraOncesi { get; set; }
+        public string ToplamDoktoraSonrasi { get; set; }
+        public string ToplamNetPuan { get; set; }
+
+        public int Colum { get; set; } = 9;
+        public string[] ErrorMessage { get; set; } 
+        
+
+    }
+    public class ListMadde
+    {
+        public string MaddeAdi { get; set; }
+        public string DoktoraOncesi { get; set; }
+        public string DoktoraSonrasi { get; set; }
+        public string NetPuan { get; set; }
+
+    }
+}
