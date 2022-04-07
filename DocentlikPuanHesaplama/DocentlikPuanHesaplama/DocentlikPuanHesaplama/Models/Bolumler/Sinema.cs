c472244f6b0ae5e1f42759a3b6dd82f73c243eb4
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-08 00:59:43 +0300

    function

---
+++ DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Models/Bolumler/Sinema.cs
@@ -0,0 +1,9 @@
+﻿namespace DocentlikPuanHesaplama.Models.Bolumler
+{
+    public class Sinema : BaseEntity
+    {
+        public string BolumAdi { get; set; } = "1. Sinema, Plastik Sanatlar, Tasarım, Geleneksel Türk Sanatları, Taşınabilir Kültür Varlıkları / Sanat Eserleri Restorasyonu ve Konservasyonu ";
+        public string ErrorMessage { get; set; } = "I. Madde 40 , II. Madde 10 ,III. Madde 15,IV madde 25 , V Madde 10  olmaldır  bu puanların 90 ı Doktora Sonrası olmalıdır";
+        public bool Error { get; set; } = false;
+    }
+}
--- DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Models/DocentModels/GuzelSanatlarDocentModel.cs
+++ DocentlikPuanHesaplama/DocentlikPuanHesaplama/DocentlikPuanHesaplama/Models/DocentModels/GuzelSanatlarDocentModel.cs
@@ -1,9 +1,23 @@
 ﻿using DocentlikPuanHesaplama.IdentityModel;
+using DocentlikPuanHesaplama.Models.Bolumler;
 
 namespace DocentlikPuanHesaplama.Models.DocentModels
 {
     public class GuzelSanatlarDocentModel
     {
+        private decimal SinemaMadde4(int sayi)
+        {
+            return sayi switch
+            {
+                var x when x == 0 => 12.5m,
+                var x when x == 1 => 6.25m,
+                var x when x == 2 => 6.25m,
+                var x when x == 3 => 2.5m,
+                var x => 1.0m
+            };
+
+        }
+
         public int? Id { get; set; } = default!;
         public string? MyUserId { get; set; } = default!;
         public MyUser? MyUser { get; set; } = default!;
@@ -15,8 +29,6 @@ namespace DocentlikPuanHesaplama.Models.DocentModels
         public int[] SinemaIEtkinlikSayisi { get; set; } = default!;
         public string? SinemaIHatirlatici { get; set; }
 
-
-
         public int[] SinemaIIDoktora { get; set; } = default!;
         public int[] SinemaIIEtkinlikSayisi { get; set; } = default!;
         public string? SinemaIIHatirlatici { get; set; }
@@ -27,10 +39,98 @@ namespace DocentlikPuanHesaplama.Models.DocentModels
         public int[] SinemaIIIEtkinlikSayisi { get; set; } = default!;
         public string? SinemaIIIHatirlatici { get; set; }
 
+      
+            private Sinema SinemaHesapla()
+        {
+            Sinema model = new();
+            decimal Madde1 = 0;
+            decimal Madde2 = 0;
+            decimal Madde3 = 0;
+            decimal Madde4 = 0;
+            decimal Madde5 = 0;
+            // ekstra değişken oluşturmamak için önce c yi kotnrol edeceğim
+            if (SinemaIDoktora.Count() > 1)
+            {
+                for (int i = 1; i < SinemaIDoktora.Count(); i++)
+                {
+                    if (SinemaIDoktora[i] == 0 && SinemaIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraOncesiPuan += 20.0m * (decimal)SinemaIEtkinlikSayisi[i];
+
+                    }
+                    else if (SinemaIDoktora[i] == 1 && SinemaIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraSonrasiPuan += 20.0m * (decimal)SinemaIEtkinlikSayisi[i];
+                        Madde1 += 20 * SinemaIEtkinlikSayisi[i];
+                    }
+                }
+            }
+
+            if (SinemaIIDoktora.Count() > 1)
+            {
+                for (int i = 1; i < SinemaIIDoktora.Count(); i++)
+                {
+                    if (SinemaIIDoktora[i] == 0 && SinemaIIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraOncesiPuan += 5.0m * (decimal)SinemaIIEtkinlikSayisi[i];
+
+                    }
+                    else if (SinemaIIDoktora[i] == 1 && SinemaIIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraSonrasiPuan += 5.0m * (decimal)SinemaIIEtkinlikSayisi[i];
+                        Madde2 += 5.0m * (decimal)SinemaIIEtkinlikSayisi[i];
+                    }
+                }
+            }
+
+            if (SinemaIIIDoktora.Count() > 1)
+            {
+                for (int i = 1; i < SinemaIIIDoktora.Count(); i++)
+                {
+                    if (SinemaIIIDoktora[i] == 0 && SinemaIIIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraOncesiPuan += 7.5m * (decimal) SinemaIIIEtkinlikSayisi[i];
+
+                    }
+                    else if (SinemaIIIDoktora[i] == 1 && SinemaIIIEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraSonrasiPuan += 7.5m * (decimal)SinemaIIIEtkinlikSayisi[i];
+                        Madde3 += 7.5m * (decimal)SinemaIIIEtkinlikSayisi[i];
+                    }
+                }
+            }
+
+
+
+
+            if (SinemaVDoktora.Count() > 1)
+            {
+                for (int i = 1; i < SinemaVDoktora.Count(); i++)
+                {
+                    if (SinemaVDoktora[i] == 0 && SinemaVEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraOncesiPuan += 5.0m * (decimal)SinemaVEtkinlikSayisi[i];
+
+                    }
+                    else if (SinemaVDoktora[i] == 1 && SinemaVEtkinlikSayisi[i] > 0)
+                    {
+                        model.HamDoktoraSonrasiPuan += 5.0m * (decimal)SinemaVEtkinlikSayisi[i];
+                        Madde5 += 5.0m * (decimal)SinemaVEtkinlikSayisi[i];
+                    }
+                }
+            }
+
+
+
+            model.NetPuan = model.HamDoktoraSonrasiPuan + model.HamDoktoraOncesiPuan;
+
+            return model;
+        }
+
 
         public int[] SinemaIVDoktora { get; set; } = default!;
         public int[] SinemaIVEtkinlikSayisi { get; set; } = default!;
-        public int[] SinemaIVTur { get; set; } = default!; /* 0-kitap 12.5  // 1-kitapçeviri 6.25 //    2-makale 6.25  / 3-makale çeviri 6.25*/
+        public int[] SinemaIVTur { get; set; } = default!; /* 0-kitap 12.5  // 1-kitapçeviri 6.25 //    2-makale 6.25  / 3-makale çeviri 2.5*/
         public string? SinemaIVHatirlatici { get; set; }
 
 
