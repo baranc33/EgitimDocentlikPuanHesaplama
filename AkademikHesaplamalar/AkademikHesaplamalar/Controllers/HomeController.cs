750eae3ccf6b724208ded163a5ffc4a36575f7ea
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-12 17:21:26 +0300

    ready

---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using AkademikHesaplamalar.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademikHesaplamalar.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public HomeController(UserManager<MyUser> userManager, SignInManager<MyUser> signInManager) : base(userManager, signInManager)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
