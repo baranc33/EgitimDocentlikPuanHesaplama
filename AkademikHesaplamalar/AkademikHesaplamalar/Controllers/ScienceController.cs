605aecafb936210475ccb9d265ab0825572f4f93
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-12 17:42:34 +0300

    next

---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Controllers/ScienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademikHesaplamalar.Controllers
+{
+    public class ScienceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
