5ce7c19bdbb3ff71b86d246648fa92a7320ac0ed
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-12 23:26:44 +0300

    s

---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Controllers/Spor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademikHesaplamalar.Controllers
+{
+    public class Spor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
