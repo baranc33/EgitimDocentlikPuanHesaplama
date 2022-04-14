328049b3a60ed6f5cc8dc92a061a2f2b1d5610ea
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-14 22:32:20 +0300

    message entity

---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace AkademikHesaplamalar.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string FullName{ get; set; } = "";
+        public string MailAdres { get; set; } = "";
+        public string Title{ get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
