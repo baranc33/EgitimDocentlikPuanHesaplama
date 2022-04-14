739818c1bbd2b11ab31c3b48e36c34e9ff269d25
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-14 22:29:55 +0300

    entity

---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Models/AdminMember.cs
@@ -0,0 +1,19 @@
+﻿namespace AkademikHesaplamalar.Models
+{
+    public class AdminMember
+    {
+        public int Id{ get; set; }
+        public int IdRow { get; set; }
+        public string FullName{ get; set; } = "";
+        public string Degree { get; set; } = "";// ünvan
+        public string Github{ get; set; } = "";
+        public string Linkedin { get; set; } = "";
+        public string Instegram{ get; set; } = "";
+        public string Facebook{ get; set; } = "";
+        public string Description{ get; set; } = "";
+        public string MailAdres{ get; set; } = "";
+        public string MailExtension{ get; set; } = "";
+        public string WebSiteUrl{ get; set; } = "";
+        public string İmage{ get; set; } = "";
+    }
+}
---
+++ AkademikHesaplamalar/AkademikHesaplamalar/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace AkademikHesaplamalar.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Adres{ get; set; } = "";
+        public string AdresHtml { get; set; } = "";
+        public string MailAdres { get; set; } = "";
+        public string MailExtension { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+    }
+}
