4343b985b690095629600fa294d8807e4c6e8935
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-20 21:55:32 +0300

    last publish

---
+++ AkademikHesaplamalar/WebMvc/Controllers/MemberTesvikController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class MemberTesvikController : Controller
+    {
+        public IActionResult TesvikHesaplama()
+        {
+            return View();
+        }
+    }
+}
---
+++ AkademikHesaplamalar/WebMvc/Controllers/TesvikController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class TesvikController : Controller
+    {
+        public IActionResult TesvikHesaplama()
+        {
+            return View();
+        }
+    }
+}
