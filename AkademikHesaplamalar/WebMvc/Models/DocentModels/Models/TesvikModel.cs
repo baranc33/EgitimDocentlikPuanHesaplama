ec78cba46aa06165e1ab415d1238325f09de1747
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-22 03:03:22 +0300

    tasarım

---
+++ AkademikHesaplamalar/WebMvc/Models/DocentModels/Models/TesvikModel.cs
@@ -0,0 +1,27 @@
+﻿namespace WebMvc.Models.DocentModels.Models
+{
+    public class TesvikModel
+    {
+        public int Id { get; set; } = default!;
+        public string? MyUserId { get; set; } = default!;
+
+
+        #region Proje
+        public int[] Proje1 { get; set; } = default!;
+        public string[] Proje1hatirlatici { get; set; } = default!;
+
+        public int[] Proje2 { get; set; } = default!;
+        public string[] Proje2hatirlatici { get; set; } = default!;
+
+        public int[] Proje3 { get; set; } = default!;
+        public string[] Proje3hatirlatici { get; set; } = default!;
+
+        public int[] Proje4 { get; set; } = default!;
+        public string[] Proje4hatirlatici { get; set; } = default!;
+
+        public int[] Proje5 { get; set; } = default!;
+        public string[] Proje5hatirlatici { get; set; } = default!;
+
+        #endregion
+    }
+}
